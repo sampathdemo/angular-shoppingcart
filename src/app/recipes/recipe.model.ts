7d3683ca47e746c55690a004e76a05e6ca6404ee
Sampath Kumar CHALLA <sampathkumarchalla1@gmail.com>
2020-10-04 14:30:37 +0200

    created lists

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,14 @@
+import { stringify } from 'querystring'
+
+export class Recipe{
+  public name: string;
+  public description : string;
+  public imagePath: string;
+
+  constructor(name: string, desc:string, imagePath:string)
+  {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+}
